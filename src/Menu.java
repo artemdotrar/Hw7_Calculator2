dba3fd261a20537e7c4c5f695eef220bb81ee79d
artemdotrar <artemivashchenko96@gmail.com>
2022-03-23 22:08:11 +0300

    v 1.5.2

--- src/Menu.java
+++ src/Menu.java
@@ -2,29 +2,31 @@ import java.util.Scanner;
 
 public class Menu {
     private final static String ERROR_MESSAGE_1 = "Некорректное значение";
-    private final static int MAX_USERS = 3;
     private boolean isCalcMenuWorking = true;
     private boolean isUserMenuWorking = true;
-    public Calculator calc = new Calculator();
-    public User[] users = new User[MAX_USERS];
-    private int countOfUsers = 0;
-    private int userLoggonId = -1;
+    private Calculator calc = new Calculator();
+    private int currentUserId = -1;
+    private UsersDataBase usersDB = new UsersDataBase();
 
     public void printUserMenu() {
         int point = inputValidInt("\n1)Регистрация\t2)Авторизация\t3)Список пользователей\n", ERROR_MESSAGE_1);
         switch (point) {
             case 1:
-                if (countOfUsers < MAX_USERS) {
-                    users[countOfUsers] = registration();
+                if (usersDB.getCountOfUsers() < usersDB.getMaxUsers()) {
+                    usersDB.registration();
                 } else {
                     System.out.println("Превышено максимальное число пользователей");
                 }
                 break;
             case 2:
-                int userId = authorization();
+                currentUserId = usersDB.authorization();
+                if (currentUserId != -1) {
+                    System.out.printf("\nЗдравствуйте, %s, добро пожаловать в калькулятор", usersDB.getUserNameById(currentUserId));
+                    isUserMenuWorking = false;
+                }
                 break;
             case 3:
-                printUsersList();
+                usersDB.printUsersList();
                 break;
             default:
                 System.out.println(ERROR_MESSAGE_1);
@@ -44,72 +46,6 @@ public class Menu {
         } else {
             System.out.println(ERROR_MESSAGE_1);
         }
-    }
-
-    private User registration() {
-        Scanner scanner = new Scanner(System.in);
-        String password = "";
-        String name = "";
-        String login = "";
-
-        while (true) {
-            System.out.println("Введите логин: ");
-            boolean isFreeLogin = true;
-            login = scanner.nextLine();
-            for (int i = 0; i < countOfUsers; i++) {
-                if (login.equals(users[i].getLogin())) {
-                    isFreeLogin = false;
-                    break;
-                }
-            }
-            if (isFreeLogin) {
-                break;
-            } else {
-                System.out.println("Этот логин уже занят");
-            }
-        }
-        System.out.println("Введите пароль: ");
-        password = scanner.nextLine();
-        System.out.println("Введите имя пользователя: ");
-        name = scanner.nextLine();
-        countOfUsers++;
-        return new User(login, name, password);
-    }
-
-    private int authorization() { //возвращает индекс юзера в массиве при успешной авторизации или -1
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите логин: ");
-        String login = scanner.nextLine();
-        int userId = -1;
-        for (int i = 0; i < countOfUsers; i++) {
-            if (users[i].getLogin().equals(login)) {
-                userId = i;
-                break;
-            }
-        }
-        if (userId == -1) {
-            System.out.println("Нет такой учетной записи");
-            return userId;
-        } else {
-
-            System.out.println("Введите пароль: ");
-            String password = scanner.nextLine();
-            if (users[userId].isTruePassword(password)) {
-                System.out.printf("\nЗдравствуйте, %s, добро пожаловать в калькулятор", users[userId].getName());
-                isUserMenuWorking = false;
-                return userId;
-            } else {
-                System.out.println("Неверный пароль");
-                return -1;
-            }
-        }
-    }
-
-    private void printUsersList() {
-        for (int i = 0; i < countOfUsers; i++) {
-            System.out.print("[" + users[i].getLogin() + " : " + users[i].getName() + "]\t");
-        }
-        System.out.println();
     }
 
     public boolean isCalcMenuWorking() {
---
+++ src/UsersDataBase.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class UsersDataBase {
+    private final int maxUsers = 3;
+    private int countOfUsers = 0;
+    private User[] users = new User[maxUsers];
+
+
+    public int getMaxUsers() {
+        return maxUsers;
+    }
+
+    public int getCountOfUsers() {
+        return countOfUsers;
+    }
+
+    public String getUserNameById(int id) {
+        return users[id].getName();
+    }
+
+    public void registration() {
+        Scanner scanner = new Scanner(System.in);
+        String password = "";
+        String name = "";
+        String login = "";
+
+        while (true) {
+            System.out.println("Введите логин: ");
+            boolean isFreeLogin = true;
+            login = scanner.nextLine();
+            for (int i = 0; i < countOfUsers; i++) {
+                if (login.equals(users[i].getLogin())) {
+                    isFreeLogin = false;
+                    break;
+                }
+            }
+            if (isFreeLogin) {
+                break;
+            } else {
+                System.out.println("Этот логин уже занят");
+            }
+        }
+        System.out.println("Введите пароль: ");
+        password = scanner.nextLine();
+        System.out.println("Введите имя пользователя: ");
+        name = scanner.nextLine();
+        users[countOfUsers] = new User(login, name, password);
+        countOfUsers++;
+    }
+
+    public int authorization() { //возвращает индекс юзера в массиве при успешной авторизации или -1
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите логин: ");
+        String login = scanner.nextLine();
+        int userId = -1;
+        for (int i = 0; i < countOfUsers; i++) {
+            if (users[i].getLogin().equals(login)) {
+                userId = i;
+                break;
+            }
+        }
+        if (userId == -1) {
+            System.out.println("Нет такой учетной записи");
+            return userId;
+        } else {
+
+            System.out.println("Введите пароль: ");
+            String password = scanner.nextLine();
+            if (users[userId].isTruePassword(password)) {
+                return userId;
+            } else {
+                System.out.println("Неверный пароль");
+                return -1;
+            }
+        }
+    }
+
+    public void printUsersList() {
+        for (int i = 0; i < countOfUsers; i++) {
+            System.out.print("[" + users[i].getLogin() + " : " + users[i].getName() + "]\t");
+        }
+        System.out.println();
+    }
+}
